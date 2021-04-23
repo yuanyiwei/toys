4468e6bce112d9d2fb996cd947a51a9e62215b1f
yuanyiwei <40692073+yuanyiwei@users.noreply.github.com>
2021-04-23 15:54:54 +0800

    wlt: fix str cmp

--- auto-wlt/auto-wlt.sh
+++ auto-wlt/auto-wlt.sh
@@ -8,12 +8,11 @@ check() {
     RESULT=$(curl -s $URL | grep "<title>redirecting to http://wlt.ustc.edu.cn</title>")
     echo $RESULT
     
-    if [ "$RESULT" -eq "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then # bug for eq
+    if [ "$RESULT" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
         echo "Down"
         curl -vso /dev/null "http://wlt.ustc.edu.cn/cgi-bin/ip?cmd=set&url=URL&type=0&exp=0&name=$NAME&password=$PASS"
-        sleep 10
-        check
-    elif [ "$RESULT" -eq "" ] ; then
+        exit -1
+    elif [ -z "$RESULT" ] ; then
         echo "OK"
         exit 0
     else 

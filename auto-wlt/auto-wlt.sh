e6ce1b594c1d691bb1f8604c7b60079f363ce42f
yuanyiwei <40692073+yuanyiwei@users.noreply.github.com>
2021-04-23 16:00:57 +0800

    wlt: add next turn check

--- auto-wlt/auto-wlt.sh
+++ auto-wlt/auto-wlt.sh
@@ -11,7 +11,19 @@ check() {
     if [ "$RESULT" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
         echo "Down"
         curl -vso /dev/null "http://wlt.ustc.edu.cn/cgi-bin/ip?cmd=set&url=URL&type=0&exp=0&name=$NAME&password=$PASS"
-        exit -1
+        sleep 5
+
+        RESULT2=$(curl -s $URL | grep "<title>redirecting to http://wlt.ustc.edu.cn</title>")
+        echo $RESULT2
+
+        if [ "$RESULT2" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
+            echo "Error"
+            exit -1
+        elif [ -z "$RESULT" ] ; then
+            echo "Get WLT"
+            exit 0
+        fi
+
     elif [ -z "$RESULT" ] ; then
         echo "OK"
         exit 0

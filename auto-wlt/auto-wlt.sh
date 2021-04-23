375319b9ff00ad29de64a9fb99247fd59977ccc6
yuanyiwei <40692073+yuanyiwei@users.noreply.github.com>
2021-04-23 16:29:16 +0800

    wlt

--- auto-wlt/auto-wlt.sh
+++ auto-wlt/auto-wlt.sh
@@ -4,32 +4,27 @@ URL="http://www.baidu.com"
 NAME="totototoro"
 PASS="totototoro"
 
-check() {
-    RESULT=$(curl -s $URL | grep "<title>redirecting to http://wlt.ustc.edu.cn</title>")
-    echo $RESULT
+RESULT=$(curl -s $URL | grep "<title>redirecting to http://wlt.ustc.edu.cn</title>")
+echo $RESULT
+if [ "$RESULT" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
+    echo "Down"
+    curl -vso /dev/null "http://wlt.ustc.edu.cn/cgi-bin/ip?cmd=set&url=URL&type=0&exp=0&name=$NAME&password=$PASS"
+    sleep 5
 
-    if [ "$RESULT" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
-        echo "Down"
-        curl -vso /dev/null "http://wlt.ustc.edu.cn/cgi-bin/ip?cmd=set&url=URL&type=0&exp=0&name=$NAME&password=$PASS"
-        sleep 5
-
-        ## next turn of check
-        RESULT2=$(curl -s $URL | grep "<title>redirecting to http://wlt.ustc.edu.cn</title>")
-        echo $RESULT2
-        if [ "$RESULT2" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
-            echo "Error"
-            exit -1
-        elif [ -z "$RESULT2" ] ; then
-            echo "Get WLT"
-            exit 0
-        fi
-
-    elif [ -z "$RESULT" ] ; then
-        echo "OK"
-        exit 0
-    else
+    ## next turn of check
+    RESULT2=$(curl -s $URL | grep "<title>redirecting to http://wlt.ustc.edu.cn</title>")
+    echo $RESULT2
+    if [ "$RESULT2" = "<title>redirecting to http://wlt.ustc.edu.cn</title>" ] ; then
         echo "Error"
+        exit -1
+    elif [ -z "$RESULT2" ] ; then
+        echo "Get WLT"
+        exit 0
     fi
-}
 
-check
+elif [ -z "$RESULT" ] ; then
+    echo "OK"
+    exit 0
+else
+    echo "Error"
+fi

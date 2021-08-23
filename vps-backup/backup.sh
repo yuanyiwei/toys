54e4d36c98000aaa6d3d44c25941bbe91710d924
totoro <totoroyyw@gmail.com>
2021-08-23 19:42:44 +0800

    update backup dir

--- vps-backup/backup.sh
+++ vps-backup/backup.sh
@@ -4,12 +4,13 @@
 # 0 6 * * * /root/backup.sh
 
 REMOTE=ustc-1drv-rw
-AREA=us
-ruLIST="/etc/ddns /etc/wireguard /root/backup"
-usLIST="/etc/ddns /etc/wireguard /root/ustc-checkin /root/backup"
+# ruLIST="/etc/ddns /etc/wireguard /root/backup"
+usLIST="/etc/ddns /etc/wireguard /root/ustc-checkin /root/backup /etc/iptables"
+jpLIST="/etc/ddns /etc/wireguard /root/backup /etc/iptables"
 
 
-for DIR in $usLIST
+AREA=jp
+for DIR in $jpLIST
 do
     rclone sync ${DIR} $REMOTE:vps/${AREA}${DIR}
     # rclone copy $REMOTE:vps/${AREA}${DIR} ${DIR} # for rollback

ce1b3b48740a5d908260806372186dbfcde74c77
totoroyyw <40692073+yuanyiwei@users.noreply.github.com>
2021-12-12 11:30:52 +0800

    debian init add wg

--- debian-init/init.sh
+++ debian-init/init.sh
@@ -23,6 +23,7 @@ apt upgrade -qqy
 ## should update to stable first and use `apt upgrade --without-new-pkgs` and `apt full-upgrade`
 apt install -qqy git wget curl zsh python3 python3-pip jq tmux vim iptables-persistent htop socat dnsutils # fail2ban (with python2.7 in buster)
 apt install -qqy nload iftop vnstat vnstati sysstat
+apt install -qqy wireguard
 ## apt install -qqy ipset ipset-persistent
 # apt install -qqy iptraf-ng mtr-tiny
 # apt install -qqy proxychains4

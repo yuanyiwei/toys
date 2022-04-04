f30992882a68f03a6a2a3adb8bafeffb1c1eb534
totoro <totoroyyw@gmail.com>
2022-04-04 18:54:09 +0800

    debian init update tune

--- debian-init/neko_tune.sh
+++ debian-init/neko_tune.sh
@@ -132,8 +132,12 @@ use_cloud(){ #使用cloud kernel
 apt -qq update && apt install -qqy linux-image-cloud-amd64
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+# cat >> /etc/sysctl.conf << EOF
+# net.core.default_qdisc=fq
+# net.ipv4.tcp_congestion_control=bbr
+# EOF
 cat >> /etc/sysctl.conf << EOF
-net.core.default_qdisc=fq
+net.core.default_qdisc=cake
 net.ipv4.tcp_congestion_control=bbr
 EOF
 sysctl -p && sysctl --system
